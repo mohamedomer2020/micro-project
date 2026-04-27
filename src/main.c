759e316780cac0f9ac02f625c6a71815ac016a03
mohamedomer2020 <m.awad200010@gmail.com>
2026-04-27 14:22:37 -0700

    Implement button input for adaptive traffic light
    add button input logic

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
  Smart Traffic Light System
  PIC16F877A - CCS C
  Description: Adaptive traffic light using buttons
+#define BTN_A PIN_B0
+#define BTN_B PIN_B1
+case S0_A_GREEN:
+if(input(BTN_A))
+delay_ms(1500);
+else
+delay_ms(1000);
+currentState = S1_A_YELLOW;
+break;
+
+case S2_B_GREEN:
+if(input(BTN_B))
+delay_ms(1500);
+else
+delay_ms(1000);
+currentState = S3_B_YELLOW;
+break;
 
